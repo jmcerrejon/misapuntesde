2992afc720d7ac4dcfc9bf5b800e348f62eb2e8d
Jose Cerrejon <jmcerrejon@sioxfer.com>
2024-02-19 16:48:25 +0100

    feat: Now is functional and create, upload and extract the content of the site

--- tools/copy2ftp.sh
+++ tools/copy2ftp.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 #
-# Copy files to FTP server
+# Description: The script is pretty straightforward. It compresses the dist/ directory,
+#              uploads it to the FTP server, extracts it, and cleans up.
+#              The .env file is used to store the FTP credentials.
+# Author     : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
 #
+# shellcheck disable=SC2029
 readonly CURRENT_YEAR=$(date +%Y)
 readonly CURRENT_MONTH=$(date +%m)
+readonly TOOLS_DIR="tools"
 readonly DIST_DIR="src/.vuepress/dist"
 readonly DIST_FILENAME="dist.tar.gz"
 readonly INCREMENTAL_UPLOAD="true"
-readonly FTP_HOST="ftp.example.com"
-readonly FTP_USER="user"
-readonly FTP_PASS="password"
-readonly FTP_DIR="/public_html"
-readonly FTP_PORT="21"
 
 if [[ ! -e ./package.json ]]; then
     echo "Please run this script from the root of the project"
+    exit 1
+fi
+
+if [[ ! -e $TOOLS_DIR/.env ]]; then
+    echo "Can't found .env file. Please create one with FTP_HOST, FTP_USER & FTP_PASS."
     exit 1
 fi
 
@@ -22,6 +27,9 @@ if [[ ! -d $DIST_DIR ]]; then
     echo "Can't found directory $DIST_DIR. Building the project..."
     npm run docs:build || exit 1
 fi
+
+# shellcheck source=/dev/null
+source "$TOOLS_DIR/.env"
 
 function clean_dist_directory() {
     echo "Cleaning dist directory..."
@@ -36,45 +44,57 @@ function compress() {
     clean_dist_directory
 
     echo "Compress in progress..."
+    cd $DIST_DIR || exit 1
 
     if [[ "$INCREMENTAL_UPLOAD" == "true" ]]; then
-        # tar all files except directories images, videos and res
-        tar -czf "./$DIST_FILENAME" "$DIST_DIR"
+        tar -czf ../../../$DIST_FILENAME .
     else
-        tar -czf "./$DIST_FILENAME" .
+        tar -czf ../../../$DIST_FILENAME .
     fi
 
+    cd - || exit 1
     echo "./$DIST_FILENAME created!"
 }
-
-function upload() {
-    ftp -n -p <<EOF
-# upload the files
-ftp -n -p <<EOF
-open $FTP_HOST $FTP_PORT
-user $FTP_USER $FTP_PASS
-cd $FTP_DIR
-binary
-put ../dist.tar
+function upload_to_ftp() {
+    if [[ ! -e $DIST_FILENAME ]]; then
+        compress
+    fi
+    echo "Uploading to FTP server..."
+    ftp ftp://"$FTP_USER:$FTP_PASS@$FTP_HOST" <<END_SCRIPT
+put ./$DIST_FILENAME /$DIST_FILENAME
 quit
-EOF
+END_SCRIPT
+}
+
+function extract_on_ftp() {
+    if [[ ! -e $DIST_FILENAME ]]; then
+        echo "$DIST_FILENAME does not exists. Aborting..."
+        exit 1
+    fi
+    echo "Extracting & deleting file on FTP server..."
+    ssh "$FTP_USER@$FTP_HOST" "tar -xzf $DIST_FILENAME && rm -rf $DIST_FILENAME"
+    echo "Done!"
 }
 
 function cleanup() {
-    rm -f dist.tar
+    [[ -e $DIST_FILENAME ]] && rm -rf ./$DIST_FILENAME
+    [[ -d $DIST_DIR ]] && rm -rf "$DIST_DIR"
 }
 
-function extract() {
-    ftp -n -p <<EOF
-# extract the files
-ftp -n -p <<EOF
-open $FTP_HOST $FTP_PORT
-user $FTP_USER $FTP_PASS
-cd $FTP_DIR
-binary
-get dist.tar
-quit
-EOF
-}
-
-compress
+# Smell code approaching, It's too late here. I'll refactor it later (Not promise).
+if [[ -e $DIST_FILENAME ]]; then
+    read -p "Do you want to delete the current $DIST_FILENAME? (Y/n) " response
+    if [[ $response =~ [Yy] ]]; then
+        rm -rf $DIST_FILENAME
+        compress
+    fi
+else
+    compress
+fi
+upload_to_ftp
+extract_on_ftp
+read -p "Do you want to delete $DIST_FILENAME & $DIST_DIR? (y/N) " response
+if [[ $response =~ [Yy] ]]; then
+    cleanup
+fi
+echo "Have a nice day!"
